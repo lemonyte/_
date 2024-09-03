7c4b321aad4b9e71d89727f6d562483a3f8c77fe
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2024-09-02 21:06:19 -0700

    Add module docstring

--- src/underscores/__init__.py
+++ src/underscores/__init__.py
@@ -1,3 +1,28 @@
+"""
+`_` allows you to write Python code using only `_`.
+
+You shouldn't need to import this module unless the .pth file fails to install.
+See the [README](https://github.com/lemonyte/_) for more information.
+
+## Usage
+
+The following script is a "Hello, world!" program which can be executed by the Python interpreter:
+
+```python
+# coding: _
+_ ____ _ _____ _ ____ __ _ _ ___ ______ ____ _ ____ _ ___ _ ____ __ ___ _ __ _
+_____ _ _ ______ _____ _ ___ _ _ _ ___ _____ ______ _ ____ _ _ _ ____ _ _ _ ____
+_ ____ _ __ __ ___ _ _ ______ ___ _ ____ __ ______ _ ____ _ ____ _ ____ __ _ _
+____ _ _ _ ___ _____ _____ _ _ ______ ____ _ _ ______ _____ _ __ _ ______
+```
+
+You can generate a script with `_`:
+
+```python
+print('print("Hello, world!")'.encode("_").decode("utf-8"))
+```
+"""
+
 from underscores._ import _
 
 __name__ = "_"
